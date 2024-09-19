9b7be7828e95b1e01dc0f825e8589bede06b1574
JYC0413 <juyichen0413@foxmail.com>
2024-09-19 23:10:51 +0800

    fix

--- webServer.py
+++ webServer.py
@@ -88,4 +88,4 @@ def download_file(filename):
     return send_from_directory(VIDEO_FOLDER, filename, as_attachment=True)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', debug=True)
