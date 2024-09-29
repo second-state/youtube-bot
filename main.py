13741019788d13a9fae7204fad32c080fa803f71
JYC0413 <juyichen0413@foxmail.com>
2024-09-29 11:18:25 +0800

    fix

--- main.py
+++ main.py
@@ -190,15 +190,15 @@ def main(second=0, youtube_link="https://www.youtube.com/watch?v=Hf9zfjflP_0", e
                     url = "https://code.flows.network/webhook/ruvTvWEtUoK0WyZq3w5y/send_email"
 
                     if language == "ja":
-                        trans_message = f"親愛なるユーザーの皆様，\n\n弊社の動画翻訳サービスをご利用いただき誠にありがとうございます。ビデオの翻訳が完了しました。翻訳されたビデオは以下のリンクからご覧いただけます：\n\n{DOMAIN}/videos/{final_video}.mp4\n\nご質問がある場合、またはさらにサポートが必要な場合は、お気軽にお問い合わせください。\n\nご支援に改めて感謝し、より質の高いサービスを提供できることを楽しみにしています！\n\n幸運を祈ります，\n\nSecond State チーム"
+                        trans_message = f"親愛なるユーザーの皆様，\n\n弊社の動画翻訳サービスをご利用いただき誠にありがとうございます。ビデオの翻訳が完了しました。翻訳されたビデオは以下のリンクからご覧いただけます：\n{DOMAIN}/videos/{final_video}\n\n字幕付きのオリジナル動画：\n{DOMAIN}/videos/{final_srt}\n\nオリジナル動画から認識されたテキスト：\n{DOMAIN}/videos/{final_en}\n\n翻訳されたテキスト：\n{DOMAIN}/videos/{final_transcript}\n\nご質問がある場合、またはさらにサポートが必要な場合は、お気軽にお問い合わせください。\n\nご支援に改めて感謝し、より質の高いサービスを提供できることを楽しみにしています！\n\n幸運を祈ります，\n\nSecond State チーム"
                     else:
-                        trans_message = f"尊敬的用户，\n\n感谢您使用我们的视频翻译服务。我们已经完成了您的视频翻译工作，您可以通过以下链接查看翻译后的视频：\n\n{DOMAIN}/videos/{final_video}.mp4\n\n原声加字幕的视频：\n\n{DOMAIN}/videos/{final_srt}.mp4\n\n原视频识别到的文字：\n\n{DOMAIN}/videos/{final_en}.txt\n\n翻译后的文字：\n\n{DOMAIN}/videos/{final_transcript}.txt\n\n如果您有任何疑问或需要进一步的帮助，请随时与我们联系。\n\n再次感谢您的支持，期待为您提供更多优质的服务！\n\n祝好，\n\nSecond State 团队"
+                        trans_message = f"尊敬的用户，\n\n感谢您使用我们的视频翻译服务。我们已经完成了您的视频翻译工作，您可以通过以下链接查看翻译后的视频：\n{DOMAIN}/videos/{final_video}\n\n原声加字幕的视频：\n{DOMAIN}/videos/{final_srt}\n\n原视频识别到的文字：\n{DOMAIN}/videos/{final_en}\n\n翻译后的文字：\n{DOMAIN}/videos/{final_transcript}\n\n如果您有任何疑问或需要进一步的帮助，请随时与我们联系。\n\n再次感谢您的支持，期待为您提供更多优质的服务！\n\n祝好，\n\nSecond State 团队"
                     data = {
                         "code": "1234",
                         "mime": "text/plain",
                         "to": email_link,
                         "subject": "您的视频翻译已完成 | Your Video Translation is Complete",
-                        "body": f"{trans_message}\n\n\nDear User,\n\nThank you for using our video translation service. We have completed the translation of your video, and you can view the translated video via the link below:\n\n{DOMAIN}/videos/{final_video}.mp4\n\nIf you have any questions or need further assistance, feel free to contact us.\n\nOnce again, thank you for your support. We look forward to serving you in the future!\n\nBest regards,\n\nSecond State Team"
+                        "body": f"{trans_message}\n\n\nDear User,\n\nThank you for using our video translation service. We have completed the translation of your video, and you can view the translated video via the link below:\n{DOMAIN}/videos/{final_video}\n\nOriginal video with subtitles:\n{DOMAIN}/videos/{final_srt}\n\nText recognized from the original video:\n{DOMAIN}/videos/{final_en}\n\nTranslated text:\n{DOMAIN}/videos/{final_transcript}\n\nIf you have any questions or need further assistance, feel free to contact us.\n\nOnce again, thank you for your support. We look forward to serving you in the future!\n\nBest regards,\n\nSecond State Team"
                     }
 
                     # 发送 POST 请求，使用 json 参数将字典自动转换为 JSON 格式
